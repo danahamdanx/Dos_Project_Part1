88fad4888501fe05e8e9cab04df7d82280356dab
danahamdanx <dana29454@gmail.com>
2025-11-04 18:29:27 +0200

    feat(order): add order service with purchase endpoint and logging

---
+++ order-service/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# URL of catalog-service (Docker will resolve this later using service name)
+CATALOG_URL = "http://localhost:5001"
+
+orders_log = "orders.txt"
+
+@app.route("/purchase/<int:item_id>", methods=["POST"])
+def purchase(item_id):
+    # Ask catalog to check stock & decrement if available
+    r = requests.post(f"{CATALOG_URL}/check_and_decrement/{item_id}")
+
+    if r.status_code == 200:
+        data = r.json()
+
+        # Save order history in a text file
+        with open(orders_log, "a") as file:
+            file.write(f"bought book id={item_id}\n")
+
+        return jsonify({
+            "success": True,
+            "message": f"Book {item_id} purchased successfully",
+            "new_quantity": data["new_quantity"]
+        })
+
+    elif r.status_code == 409:
+        return jsonify({"success": False, "message": "Out of stock"}), 409
+
+    else:
+        return jsonify({"success": False, "message": "Book not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(port=5002, debug=True)
