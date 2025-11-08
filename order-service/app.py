9675a8ff5c3cf0d608ee62325a7989fc0242971e
Fatima1Nasser <s12112649@stu.najah.edu>
2025-11-08 21:46:09 +0200

    design the page

--- order-service/app.py
+++ order-service/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 CORS(app)
 
 # URL of catalog-service (Docker will resolve this later using service name)
-#CATALOG_URL = "http://localhost:5001"
-CATALOG_URL = "http://catalog-service:5001"
+CATALOG_URL = "http://localhost:5001"
+#CATALOG_URL = "http://catalog-service:5001"
 
 orders_log = "orders.txt"
 
